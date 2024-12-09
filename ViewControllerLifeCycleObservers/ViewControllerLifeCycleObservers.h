8dc9a5d0ad6f57eb7fa52f33214832723a8476f7
Ario <ariobarxan.dev@gmail.com>
2024-12-09 15:16:31 +1100

    Initial Commit

---
+++ ViewControllerLifeCycleObservers/ViewControllerLifeCycleObservers.h
@@ -0,0 +1,18 @@
+//
+//  ViewControllerLifeCycleObservers.h
+//  ViewControllerLifeCycleObservers
+//
+//  Created by Ario Zarepour on 9/12/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ViewControllerLifeCycleObservers.
+FOUNDATION_EXPORT double ViewControllerLifeCycleObserversVersionNumber;
+
+//! Project version string for ViewControllerLifeCycleObservers.
+FOUNDATION_EXPORT const unsigned char ViewControllerLifeCycleObserversVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewControllerLifeCycleObservers/PublicHeader.h>
+
+
