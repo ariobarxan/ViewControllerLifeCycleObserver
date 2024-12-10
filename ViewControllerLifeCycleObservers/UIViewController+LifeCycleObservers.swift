89c610b25ca9dda4fc881b31811fb6a423d8b82f
Ario <ariobarxan.dev@gmail.com>
2024-12-10 14:27:51 +1100

    Add xcode documentation for onViewWillAppear event

--- ViewControllerLifeCycleObservers/UIViewController+LifeCycleObservers.swift
+++ ViewControllerLifeCycleObservers/UIViewController+LifeCycleObservers.swift
@@ -5,6 +5,34 @@ public protocol UIViewControllerLifecycleObserver {
 }
 
 public extension UIViewController {
+    /// Adds a lifecycle observer to the view controller that executes a callback
+    /// whenever `viewWillAppear(_:)` is called on the view controller.
+    ///
+    /// Use this method to run custom code when the view controller's view is about
+    /// to appear. The observer triggers the provided callback during the
+    /// `viewWillAppear(_:)` lifecycle event and can be removed if needed.
+    /// - Parameter callback: A closure to execute when `viewWillAppear(_:)` is called.
+    /// - Returns: A ```UIViewControllerLifecycleObserver``` object that manages the observer.
+    ///
+    /// Example usage:
+    /// ```swift
+    /// override func viewDidLoad() {
+    ///     super.viewDidLoad()
+    ///
+    ///     // Attach an observer to run code during `viewWillAppear(_:)`
+    ///     let observer = onViewWillAppear {
+    ///         print("View is about to appear!")
+    ///     }
+    ///
+    ///     // Optionally store the observer to remove it later
+    ///     self.lifecycleObserver = observer
+    /// }
+    ///
+    /// var lifecycleObserver: UIViewControllerLifecycleObserver?
+    /// ```
+    /// Call `remove()` on this object to detach the observer when no longer needed.
+    ///
+    ///  > Important: The callback is executed every time `viewWillAppear(_:)` is called. To prevent memory leaks, ensure you remove the observer when it's no longer needed, such as in `deinit` or `viewWillDisappear(_:)`.)
     @discardableResult
     func onViewWillAppear(run callback: @escaping () -> Void) -> UIViewControllerLifecycleObserver {
         let observer = ViewControllerLifeCycleObserver(viewWillApearCallBack: callback)
